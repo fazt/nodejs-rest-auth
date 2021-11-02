3017bac4a84453bebcdb3be4d3c820ac45da4684
fazt <fazt@faztweb.com>
2021-11-02 16:10:29 -0500

    User Model added

---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+import { Schema, model } from 'mongoose'
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+export default model('User', userSchema)
